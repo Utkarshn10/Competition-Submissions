48618c5c51cbadb6fc2473830df89544f30dd3dc
Utkarsh <utkarshnaagr2000@gmail.com>
2020-10-29 21:01:00 +0530

    Add solution for problem `B - Integer Preference` Link: https://atcoder.jp/contests/abl/tasks/abl_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abl/submissions/17722707

---
+++ atcoder/abl/B.java
@@ -0,0 +1,99 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	   static class FastReader 
+	    { 
+       BufferedReader br; 
+       StringTokenizer st; 
+      
+ 
+       public FastReader() 
+       { 
+           br = new BufferedReader(new
+                    InputStreamReader(System.in)); 
+       } 
+ 
+       String next() 
+       { 
+           while (st == null || !st.hasMoreTokens()) 
+           { 
+               try
+               { 
+                   st = new StringTokenizer(br.readLine()); 
+               } 
+               catch (IOException  e) 
+               { 
+                   e.printStackTrace(); 
+               } 
+           } 
+           return st.nextToken();
+           
+       } 
+ 
+       int nextInt() 
+       { 
+           return Integer.parseInt(next()); 
+       } 
+ 
+       long nextLong() 
+       { 
+           return Long.parseLong(next()); 
+       } 
+ 
+       double nextDouble() 
+       { 
+           return Double.parseDouble(next()); 
+       } 
+ 
+       String nextLine() 
+       { 
+           String str = ""; 
+           try
+           { 
+               str = br.readLine(); 
+           } 
+           catch (IOException e) 
+           { 
+               e.printStackTrace(); 
+           } 
+           return str; 
+       }
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public BigInteger nextBigInteger() {
+			// TODO Auto-generated method stub
+			return null;
+		} 
+   } 
+	public static void main(String[] args) {
+		FastReader s=new FastReader();
+		long a = s.nextLong();
+		long b = s.nextLong();
+		long c = s.nextLong();
+		long d = s.nextLong();
+		long min = Math.min(b, d);
+		long max = Math.max(a,c);
+		if((max-min)<=0)System.out.println("Yes");
+		else System.out.println("No");
+
+				
+	}	
+
+}
+		
+	
+		
