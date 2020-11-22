f6dd897d2dfd736536816f64f480e7d541b36b4a
Utkarsh <utkarshnaagr2000@gmail.com>
2020-11-22 17:31:00 +0530

    Add solution for problem `A - Determinant` Link: https://atcoder.jp/contests/abc184/tasks/abc184_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc184/submissions/18305758

---
+++ atcoder/abc184/A.java
@@ -0,0 +1,108 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	   static class FastReader 
+	    { 
+       BufferedReader br; 
+       StringTokenizer st; 
+      
+ 
+       public FastReader() 
+       { 
+           br = new BufferedReader(new
+                    InputStreamReader(System.in)); 
+       } 
+ 
+       String next() 
+       { 
+           while (st == null || !st.hasMoreTokens()) 
+           { 
+               try
+               { 
+                   st = new StringTokenizer(br.readLine()); 
+               } 
+               catch (IOException  e) 
+               { 
+                   e.printStackTrace(); 
+               } 
+           } 
+           return st.nextToken();
+           
+       } 
+ 
+       int nextInt() 
+       { 
+           return Integer.parseInt(next()); 
+       } 
+ 
+       long nextLong() 
+       { 
+           return Long.parseLong(next()); 
+       } 
+ 
+       double nextDouble() 
+       { 
+           return Double.parseDouble(next()); 
+       } 
+ 
+       String nextLine() 
+       { 
+           String str = ""; 
+           try
+           { 
+               str = br.readLine(); 
+           } 
+           catch (IOException e) 
+           { 
+               e.printStackTrace(); 
+           } 
+           return str; 
+       }
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public BigInteger nextBigInteger() {
+			// TODO Auto-generated method stub
+			return null;
+		} 
+   } 	
+	   static Map<Integer,Integer> ar = new HashMap<>();
+	   static boolean[] v = new boolean[100005];
+	   static void dfs(int i) {
+		   if(!v[i]) {
+			   v[i]=true;
+			   for(int a: ar.values()) {
+				   dfs(a); 	
+			   }
+		   }
+	   }
+
+	public static void main(String[] args) {
+		FastReader s=new FastReader();
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		int d = s.nextInt();
+		System.out.println(a*d-b*c);
+	}	
+
+}
+		
+	
+		
