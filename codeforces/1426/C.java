ad714253749cf925051fbd591790f3b5f65f2048
Utkarsh <utkarshnaagr2000@gmail.com>
2020-11-23 18:07:00 +0530

    Add solution for problem `C - Increase and Copy` Link: https://codeforces.com/contest/1426/problem/C Tags: binary search, constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1426/submission/99320693

--- codeforces/1426/C.java
+++ codeforces/1426/C.java
@@ -1,160 +1,135 @@
 
-		
-		import java.io.BufferedReader;
-		import java.io.IOException;
-		import java.io.InputStreamReader;
-		import java.math.BigInteger;
-	import java.util.ArrayList;
-	import java.util.Arrays;
-	import java.util.HashSet;
-	import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-	import java.util.StringTokenizer;
 
-	
-	
-		
-		
-		
-		public class main1 {
-			static class FastReader 
-		    { 
-		   BufferedReader br; 
-		   StringTokenizer st; 
-		  
-		
-		   public FastReader() 
-		   { 
-		       br = new BufferedReader(new
-		                InputStreamReader(System.in)); 
-		   } 
-		
-		   String next() 
-		   { 
-		       while (st == null || !st.hasMoreTokens()) 
-		       { 
-		           try
-		           { 
-		               st = new StringTokenizer(br.readLine()); 
-		           } 
-		           catch (IOException  e) 
-		           { 
-		               e.printStackTrace(); 
-		           } 
-		       } 
-		       return st.nextToken();
-		       
-		   } 
-		
-		   int nextInt() 
-		   { 
-		       return Integer.parseInt(next()); 
-		   } 
-		
-		   long nextLong() 
-		   { 
-		       return Long.parseLong(next()); 
-		   } 
-		
-		   double nextDouble() 
-		   { 
-		       return Double.parseDouble(next()); 
-		   } 
-		
-		   String nextLine() 
-		   { 
-		       String str = ""; 
-		       try
-		       { 
-		           str = br.readLine(); 
-		       } 
-		       catch (IOException e) 
-		       { 
-		           e.printStackTrace(); 
-		       } 
-		       return str; 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	   static class FastReader 
+	    { 
+       BufferedReader br; 
+       StringTokenizer st; 
+      
+ 
+       public FastReader() 
+       { 
+           br = new BufferedReader(new
+                    InputStreamReader(System.in)); 
+       } 
+ 
+       String next() 
+       { 
+           while (st == null || !st.hasMoreTokens()) 
+           { 
+               try
+               { 
+                   st = new StringTokenizer(br.readLine()); 
+               } 
+               catch (IOException  e) 
+               { 
+                   e.printStackTrace(); 
+               } 
+           } 
+           return st.nextToken();
+           
+       } 
+ 
+       int nextInt() 
+       { 
+           return Integer.parseInt(next()); 
+       } 
+ 
+       long nextLong() 
+       { 
+           return Long.parseLong(next()); 
+       } 
+ 
+       double nextDouble() 
+       { 
+           return Double.parseDouble(next()); 
+       } 
+ 
+       String nextLine() 
+       { 
+           String str = ""; 
+           try
+           { 
+               str = br.readLine(); 
+           } 
+           catch (IOException e) 
+           { 
+               e.printStackTrace(); 
+           } 
+           return str; 
+       }
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public BigInteger nextBigInteger() {
+			// TODO Auto-generated method stub
+			return null;
+		} 
+   } 	
+	   static Map<Integer,Integer> ar = new HashMap<>();
+	   static boolean[] v = new boolean[100005];
+	   static void dfs(int i) {
+		   if(!v[i]) {
+			   v[i]=true;
+			   for(int a: ar.values()) {
+				   dfs(a); 	
+			   }
 		   }
-		
-			public Character charAt(int i) {
-				// TODO Auto-generated method stub
-				return null;
+	   }
+//		int r1 =s.nextInt();
+//		int c1 =s.nextInt();
+//		int r2 =s.nextInt();
+//		int c2 =s.nextInt();
+//		if(r1==c1&&c1==c2 && c2==r2)System.out.println(0);
+//		else if(r1==c1&&r2==c2)System.out.println(1);
+//		else if(r1==c1)System.out.println(2);
+//		else {
+//			if(r1+c1==c2+r2)System.out.println(1);
+//			else if(r1-c1==r2-c2)System.out.println(1);
+//			else if(Math.abs(r1-r1)+Math.abs(c1-c2)<3 || Math.abs(r1-r1)+Math.abs(c1-c2)==3 )System.out.println(1);
+//			else System.out.println(3);
+//		}
+//		int a  =s.nextInt();
+//		int b  =s.nextInt();
+//		int c  =s.nextInt();
+//		
+//		double a1 =((double)a/(double)(a+b+c));
+////		System.out.println(a1);
+//		double b1 =((double)b/(double)(a+b+c));
+//		double c1 =((double)c/(double)(a+b+c));
+//		System.out.println((double)(100-a)*(a1)+(100-b)*b1+(100-c)*c1);
+	public static void main(String[] args) {
+		FastReader s=new FastReader();
+		int t = s.nextInt();
+		while(t-->0) {
+			int n = s.nextInt();
+			int ans=Integer.MAX_VALUE;
+			for(int i=1;i*i<=n;i++) {
+				ans = (int) Math.min(ans, i-1+Math.ceil((double)(n-i)/(double)i));
 			}
+			System.out.println(ans);
+
+		}
 		
-			public BigInteger nextBigInteger() {
-				// TODO Auto-generated method stub
-				return null;
-			} 
-		} 	
-			
-		static int gcd(int a, int b)
-		{
-		    if (b==0)
-		    return a;
-		    return gcd(b, a%b);
-		}
-		static int pour(int fromCap, int toCap, int d)
-		{
-
-		    int from = fromCap;
-		    int to = 0;
-		 
-
-		    int step = 1; 
-
-		    while (from != d && to != d)
-		    {
-		        int temp = Math.min(from, toCap - to);
-		        to   += temp;
-		        from -= temp;
-		        step++;
-		        if (from == d || to == d)
-		            break;
-		 
-		        if (from == 0)
-		        {
-		            from = fromCap;
-		            step++;
-		        }
-		
-		        if (to == toCap)
-		        {
-		            to = 0;
-		            step++;
-		        }
-		    }
-		    return step;
-		}
-		static int minSteps(int m, int n, int d)
-		{
-
-		    if (m > n) {
-		    	int temp= m;
-		    	m=n;
-		    	n=temp;
-		    }
-	
-		    if (d > n) return -1;
-
-		    if ((d % gcd(n,m)) != 0)
-		        return -1;
-			
-		    return Math.min(pour(n,m,d),   // n to m
-		               pour(m,n,d));  // m to n
-		}
-
-		public static void main(String[] args) {
-				FastReader s=new FastReader();
-				int t = s.nextInt();
-				while(t-->0) {
-					int n = s.nextInt();
-					long ans=Integer.MAX_VALUE;
-					for(int x=1;x*x<=n;x++) {
-						 ans=Math.min(ans, (x-1)+(n-1)/x);
-					}
-					System.out.println(ans);
-				}
-		}
 	}	
 
+}
+		
 	
+		
