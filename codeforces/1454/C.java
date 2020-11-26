97df230743f2a48ade60d022ad1d55b7a0ea51d3
Utkarsh <utkarshnaagr2000@gmail.com>
2020-11-26 09:10:00 +0530

    Add solution for problem `C - Sequence Transformation` Link: https://codeforces.com/contest/1454/problem/C Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1454/submission/99605275

--- codeforces/1454/C.java
+++ codeforces/1454/C.java
@@ -1,160 +1,154 @@
 
+
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;	
+import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Set;
 import java.util.StringTokenizer;
-	
-public class main1 {
-	
-	
 
-	static void main() throws Exception{
-		int n=s.nextInt();
-		
-		int[] cnt = new int[n+1];
-		List<Integer> l =new ArrayList<>();
-		l.add(s.nextInt());
-		int j=1;
-		for(int i=1;i<n;i++) {
-			int p=s.nextInt();
-			if(l.get(j-1)!=p) {
-				l.add(p);
-				j++;
+
+public class Main {
+	   static class FastReader 
+	    { 
+       BufferedReader br; 
+       StringTokenizer st; 
+      
+ 
+       public FastReader() 
+       { 
+           br = new BufferedReader(new
+                    InputStreamReader(System.in)); 
+       } 
+ 
+       String next() 
+       { 
+           while (st == null || !st.hasMoreTokens()) 
+           { 
+               try
+               { 
+                   st = new StringTokenizer(br.readLine()); 
+               } 
+               catch (IOException  e) 
+               { 
+                   e.printStackTrace(); 
+               } 
+           } 
+           return st.nextToken();
+           
+       } 
+ 
+       int nextInt() 
+       { 
+           return Integer.parseInt(next()); 
+       } 
+ 
+       long nextLong() 
+       { 
+           return Long.parseLong(next()); 
+       } 
+ 
+       double nextDouble() 
+       { 
+           return Double.parseDouble(next()); 
+       } 
+ 
+       String nextLine() 
+       { 
+           String str = ""; 
+           try
+           { 
+               str = br.readLine(); 
+           } 
+           catch (IOException e) 
+           { 
+               e.printStackTrace(); 
+           } 
+           return str; 
+       }
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public BigInteger nextBigInteger() {
+			// TODO Auto-generated method stub
+			return null;
+		} 
+   } 	
+	   static Map<Integer,Integer> ar = new HashMap<>();
+	   static boolean[] v = new boolean[100005];
+	   static void dfs(int i) {
+		   if(!v[i]) {
+			   v[i]=true;
+			   for(int a: ar.values()) {
+				   dfs(a); 	
+			   }
+		   }
+	   }
+//		int r1 =s.nextInt();
+//		int c1 =s.nextInt();
+//		int r2 =s.nextInt();
+//		int c2 =s.nextInt();
+//		if(r1==c1&&c1==c2 && c2==r2)System.out.println(0);
+//		else if(r1==c1&&r2==c2)System.out.println(1);
+//		else if(r1==c1)System.out.println(2);
+//		else {
+//			if(r1+c1==c2+r2)System.out.println(1);
+//			else if(r1-c1==r2-c2)System.out.println(1);
+//			else if(Math.abs(r1-r1)+Math.abs(c1-c2)<3 || Math.abs(r1-r1)+Math.abs(c1-c2)==3 )System.out.println(1);
+//			else System.out.println(3);
+//		}
+//		int a  =s.nextInt();
+//		int b  =s.nextInt();
+//		int c  =s.nextInt();
+//		
+//		double a1 =((double)a/(double)(a+b+c));
+////		System.out.println(a1);
+//		double b1 =((double)b/(double)(a+b+c));
+//		double c1 =((double)c/(double)(a+b+c));
+//		System.out.println((double)(100-a)*(a1)+(100-b)*b1+(100-c)*c1);
+	public static void main(String[] args) {
+		FastReader s=new FastReader();
+		int t = s.nextInt();
+		while(t-->0) {
+			int n = s.nextInt();
+			int[] ar = new int[n];
+			int[] b = new int[n+1];
+			List<Integer> l = new ArrayList<>();
+			Map<Integer,Integer> m= new HashMap<Integer,Integer>();
+			int f=0;
+			for(int i=0;i<n;i++) {
+				ar[i] = s.nextInt();
 			}
+			Arrays.fill(b, 1);
+			b[ar[0]]--;
+
+			if (b[ar[0]] != b[ar[n - 1]])
+				b[ar[n - 1]]--;
+			for(int i=1;i<n;i++) {
+				if(ar[i]!=ar[i-1])b[ar[i]]++;
+			}
+			if(b[ar[0]]!=b[ar[n-1]])b[ar[0]]++;
+			int min=Integer.MAX_VALUE;
+			for(int i=0;i<n;i++) {
+//				System.out.println(b[ar[i]]);
+				min=Math.min(min, b[ar[i]]);
+			}
+			
+			System.out.println(min);
 		}
-		int c=Integer.MAX_VALUE;
-		n=l.size();
-		Arrays.fill(cnt, 1);
-		for(int i=0;i<n;i++) {
-			cnt[l.get(i)]++;
-		}
-		cnt[l.get(0)]--;
-		cnt[l.get(n-1)]--;
-		for(int i=0;i<n;i++) {
-			c=Math.min(c, cnt[l.get(i)]);
-		}
-		System.out.println(c);
+
+			
+	}	
+
+}
 		
-	}
-	public static void main(String[] args) throws Exception {
-		s = new MScanner(System.in);
-		pw = new PrintWriter(System.out);
-		int tc = 1;
-		tc	=s.nextInt();
-		for (int test = 1; test <= tc; test++) {
-				main();
-		}
-		pw.flush();
-	}
 	
-	static PrintWriter pw;
-	static MScanner s;
-	static class MScanner {
-		StringTokenizer st;
-		BufferedReader br;
-		public MScanner(InputStream system) {
-			br = new BufferedReader(new InputStreamReader(system));
-		}
-	 	public MScanner(String file) throws Exception {
-			br = new BufferedReader(new FileReader(file));
-		}
-	 
-		public String next() throws IOException {
-			while (st == null || !st.hasMoreTokens())
-				st = new StringTokenizer(br.readLine());
-			return st.nextToken();
-		}
-		public int[] intArr(int n) throws IOException {
-			int[] in = new int[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			return in;
-		}
-	 	public long[] longArr(int n) throws IOException {
-			long[] in = new long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			return in;
-		}
-		public int[] intSortedArr(int n) throws IOException {
-			int[] in = new int[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			shuffle(in);
-			Arrays.sort(in);
-			return in;
-		}
-		public long[] longSortedArr(int n) throws IOException {
-			long[] in = new long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			shuffle(in);
-			Arrays.sort(in);
-			return in;
-		}
-	 	public Integer[] IntegerArr(int n) throws IOException {
-			Integer[] in = new Integer[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			return in;
-		}
-		public Long[] LongArr(int n) throws IOException {
-			Long[] in = new Long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			return in;
-		}
-		public String nextLine() throws IOException {
-			return br.readLine();
-		}
-		public int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
-		public double nextDouble() throws IOException {
-			return Double.parseDouble(next());
-		}
-		public char nextChar() throws IOException {
-			return next().charAt(0);
-		}
-		public long nextLong() throws IOException {
-				return Long.parseLong(next());
-		}
-	 	public boolean ready() throws IOException {
-				return br.ready();
-		}
-		public void waitForInput() throws InterruptedException {
-				Thread.sleep(3000);
-		}
-	}
-		static void shuffle(int[] in) {
-			for (int i = 0; i < in.length; i++) {
-				int idx = (int) (Math.random() * in.length);
-				int tmp = in[i];
-				in[i] = in[idx];
-				in[idx] = tmp;
-			}
-	}
-	 	static void shuffle(long[] in) {
-	 		for (int i = 0; i < in.length; i++) {
-				int idx = (int) (Math.random() * in.length);
-				long tmp = in[i];
-				in[i] = in[idx];
-				in[idx] = tmp;
-			}
-		}
-	}
+		
