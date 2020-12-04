02a75eab7089ede348ac5873c7f5799b749e88e6
Utkarsh <utkarshnaagr2000@gmail.com>
2020-12-04 10:56:00 +0530

    Add solution for problem `A - Reorder` Link: https://codeforces.com/contest/1436/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1436/submission/100319009

---
+++ codeforces/1436/A.java
@@ -0,0 +1,109 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	   static class FastReader 
+	    { 
+       BufferedReader br; 
+       StringTokenizer st; 
+      
+ 
+       public FastReader() 
+       { 
+           br = new BufferedReader(new
+                    InputStreamReader(System.in)); 
+       } 
+ 
+       String next() 
+       { 
+           while (st == null || !st.hasMoreTokens()) 
+           { 
+               try
+               { 
+                   st = new StringTokenizer(br.readLine()); 
+               } 
+               catch (IOException  e) 
+               { 
+                   e.printStackTrace(); 
+               } 
+           } 
+           return st.nextToken();
+           
+       } 
+ 
+       int nextInt() 
+       { 
+           return Integer.parseInt(next()); 
+       } 
+ 
+       long nextLong() 
+       { 
+           return Long.parseLong(next()); 
+       } 
+ 
+       double nextDouble() 
+       { 
+           return Double.parseDouble(next()); 
+       } 
+ 
+       String nextLine() 
+       { 
+           String str = ""; 
+           try
+           { 
+               str = br.readLine(); 
+           } 
+           catch (IOException e) 
+           { 
+               e.printStackTrace(); 
+           } 
+           return str; 
+       }
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public BigInteger nextBigInteger() {
+			// TODO Auto-generated method stub
+			return null;
+		} 
+   } 	
+
+//	      int a = 3125, m = 9987; 
+	 //   System.out.println( modInverse(a, m));
+	   
+	   
+
+	public static void main(String[] args) {
+		FastReader s=new FastReader();
+		int t = s.nextInt();
+		while(t-->0) {
+			int n = s.nextInt();
+			int m = s.nextInt();
+			int sum=0;
+			for(int i=0;i<n;i++)sum+=s.nextInt();
+			if(sum==m)System.out.println("YES");
+			else System.out.println("NO");
+		}
+	}
+}
+
+	
+//
+//}
+		
+	
+		
