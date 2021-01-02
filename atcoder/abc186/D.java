4e809e7a0be21f746a464dcc741c089d54072a4b
Utkarsh <utkarshnaagr2000@gmail.com>
2021-01-02 12:23:00 +0530

    Add solution for problem `D - Sum of difference` Link: https://atcoder.jp/contests/abc186/tasks/abc186_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc186/submissions/19110987

---
+++ atcoder/abc186/D.java
@@ -0,0 +1,97 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;	
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class Main {
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+       			}
+			return str;
+		}
+
+		public Character charAt(int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		// public BigInteger nextBigInteger() {
+		// // TODO Auto-generated method stub
+		// return null;
+		// }
+	}
+
+	static long check(long[] a,int n) {
+		long sum=0;
+		for(int i=0;i<n;i++)sum+=(i*a[i])-(a[i]*(n-i-1));
+		return sum;
+	}
+	public static void main(String[] args) throws Exception {
+		
+		
+		FastReader s = new FastReader();
+		int n=s.nextInt();
+		long[] a = new long[n];
+		for(int i=0;i<n;i++ ) {
+			a[i]=s.nextInt();
+		}
+		Arrays.sort(a);
+		long  x= check(a,n);
+		System.out.println(x);
+	}
+}
