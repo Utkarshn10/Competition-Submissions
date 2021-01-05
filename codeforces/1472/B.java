4c1db0e64f033baa44a3beb9acd3d68691d9251f
Utkarsh <utkarshnaagr2000@gmail.com>
2021-01-05 15:33:00 +0530

    Add solution for problem `B - Fair Division` Link: https://codeforces.com/contest/1472/problem/B Tags: dp, greedy, math, *800 Ref: https://codeforces.com/contest/1472/submission/103366749

---
+++ codeforces/1472/B.java
@@ -0,0 +1,147 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;	
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+	
+public class main1 {
+	
+	
+	static int mx= 200005 ;
+	static void main() throws Exception{
+		int n=s.nextInt();
+		Integer[] a =new Integer[n];
+		long sum=0;
+		for(int i=0;i<n;i++) {
+			a[i]=s.nextInt();
+			
+		}
+		Arrays.sort(a);
+		for(int i=n-1;i>=0;i--) {
+			if(sum<=0)sum+=a[i];
+			else sum-=a[i];
+		}
+		 System.out.println(sum==0?"YES":"NO");
+	}
+	public static void main(String[] args) throws Exception {
+		s = new MScanner(System.in);
+		pw = new PrintWriter(System.out);
+		int tc = 1;
+		tc	=s.nextInt();
+		for (int test = 1; test <= tc; test++) {
+				main();
+		}
+		pw.flush();
+	}
+	
+	static PrintWriter pw;
+	static MScanner s;
+	static class MScanner {
+		StringTokenizer st;
+		BufferedReader br;
+		public MScanner(InputStream system) {
+			br = new BufferedReader(new InputStreamReader(system));
+		}
+	 	public MScanner(String file) throws Exception {
+			br = new BufferedReader(new FileReader(file));
+		}
+	 
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+		public int[] intArr(int n) throws IOException {
+			int[] in = new int[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextInt();
+			return in;
+		}
+	 	public long[] longArr(int n) throws IOException {
+			long[] in = new long[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextLong();
+			return in;
+		}
+		public int[] intSortedArr(int n) throws IOException {
+			int[] in = new int[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextInt();
+			shuffle(in);
+			Arrays.sort(in);
+			return in;
+		}
+		public long[] longSortedArr(int n) throws IOException {
+			long[] in = new long[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextLong();
+			shuffle(in);
+			Arrays.sort(in);
+			return in;
+		}
+	 	public Integer[] IntegerArr(int n) throws IOException {
+			Integer[] in = new Integer[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextInt();
+			return in;
+		}
+		public Long[] LongArr(int n) throws IOException {
+			Long[] in = new Long[n];
+			for (int i = 0; i < n; i++)
+				in[i] = nextLong();
+			return in;
+		}
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
+		public char nextChar() throws IOException {
+			return next().charAt(0);
+		}
+		public long nextLong() throws IOException {
+				return Long.parseLong(next());
+		}
+	 	public boolean ready() throws IOException {
+				return br.ready();
+		}
+		public void waitForInput() throws InterruptedException {
+				Thread.sleep(3000);
+		}
+	}
+		static void shuffle(int[] in) {
+			for (int i = 0; i < in.length; i++) {
+				int idx = (int) (Math.random() * in.length);
+				int tmp = in[i];
+				in[i] = in[idx];
+				in[idx] = tmp;
+			}
+	}
+	 	static void shuffle(long[] in) {
+	 		for (int i = 0; i < in.length; i++) {
+				int idx = (int) (Math.random() * in.length);
+				long tmp = in[i];
+				in[i] = in[idx];
+				in[idx] = tmp;
+			}
+		}
+	}
