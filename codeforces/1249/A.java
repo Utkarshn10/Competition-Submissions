4a15c13f8fd3edf788a09500408a5c3de6c2ceb9
Utkarsh <utkarshnaagr2000@gmail.com>
2021-01-23 13:04:00 +0530

    Add solution for problem `A - Yet Another Dividing into Teams` Link: https://codeforces.com/contest/1249/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1249/submission/105128712

--- codeforces/1249/A.java
+++ codeforces/1249/A.java
@@ -1,173 +1,143 @@
+	
+	
+	import java.io.BufferedReader;
+	import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.Collections;
+	import java.util.HashMap;
+	import java.util.HashSet;
+	import java.util.List;
+	import java.util.Map;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.Scanner;
+	import java.util.Set;
+	import java.util.StringTokenizer;
+import java.util.Vector;
+	
+	
+	
+	
+	public class ForLooops {
+		 static class FastReader 
+		    { 
+		        BufferedReader br; 
+		        StringTokenizer st; 
+		       
+		  
+		        public FastReader() 
+		        { 
+		            br = new BufferedReader(new
+		                     InputStreamReader(System.in)); 
+		        } 
+		  
+		        String next() 
+		        { 
+		            while (st == null || !st.hasMoreTokens()) 
+		            { 
+		                try
+		                { 
+		                    st = new StringTokenizer(br.readLine()); 
+		                } 
+		                catch (IOException  e) 
+		                { 
+		                    e.printStackTrace(); 
+		                } 
+		            } 
+		            return st.nextToken();
+		            
+		        } 
+		  
+		        int nextInt() 
+		        { 
+		            return Integer.parseInt(next()); 
+		        } 
+		  
+		        long nextLong() 
+		        { 
+		            return Long.parseLong(next()); 
+		        } 
+		  
+		        double nextDouble() 
+		        { 
+		            return Double.parseDouble(next()); 
+		        } 
+		  
+		        String nextLine() 
+		        { 
+		            String str = ""; 
+		            try
+		            { 
+		                str = br.readLine(); 
+		            } 
+		            catch (IOException e) 
+		            { 
+		                e.printStackTrace(); 
+		            } 
+		            return str; 
+		        }
+	
+				public Character charAt(int i) {
+					// TODO Auto-generated method stub
+					return null;
+				} 
+		    }
+	
+		 static int gcd(int a , int b) {
+			 if(b==0)return a;
+			 return gcd(b,a%b);
+		 }
+	
+		/**
+		 * @param args
+		 * @throws IOException
+		 */
+		 static int m =(int) (1e9+7);
+		 static long mod(long x) {
+			 
+			 return ((x%m+m)%m);
+		 }
+		 static long add(long x,long y) {
+			 return mod((mod(x)+mod(y)));
+		 }
+		 static long mul(long x,long y) {
+			 return mod((mod(x)*mod(y)));
+		 }
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.StringTokenizer;
-
-
-
-public class A {
-	static int mx= 200005 ;
-
-	static void solve() throws Exception{
-		MScanner sc = new MScanner(System.in);
-		int n=s.nextInt();
-		int[] a = new int[n];
-		for(int i=0;i<n;i++) {
-			a[i]=s.nextInt();
-//			System.out.println(a[i]);
+		public static void main(String[] args)throws IOException  {
+			FastReader s=new FastReader();
+			int t = s.nextInt();
+			while(t-->0) {
+				int n = s.nextInt();
+				int[] ar = new int[n];
+				for(int i=0;i<n;i++) {
+					ar[i] = s.nextInt();
+					
+				}
+				Arrays.sort(ar);
+				int c=0;
+				int flag=0;
+				for(int j=0;j<n && flag==0;j++) {
+					for(int i=0;i<n && flag==0;i++) {
+						if((ar[i]-ar[j])==1)flag=1;
+					}
+				}
+				
+				if(flag==1) System.out.println("2");
+				else System.out.println("1");
+					
+			
+				
+				
 		}
+			
+			
+			
 		
-		Arrays.sort(a);
-		
-		int f=0;
-		for(int i=0;i<n-1;i++) {
-//			System.out.println(a[i]);
-			if(a[i+1]-a[i]==1) {
-				f=1;
-			}
-		}
-		if(f==1)System.out.println("2");
-		else System.out.println(1);
+				
 			
 		}
-	
-	static int gcd(int a,int b) {
-		if(a==0)return b;
-		return gcd(b%a,a);
 	}
 	
-public static void main(String[] args) throws Exception {
-		s = new MScanner(System.in);
-		pw = new PrintWriter(System.out);
-		int tc = 1;
 		
-		tc	=s.nextInt();
-		for (int test = 1; test <= tc; test++) {
-				solve();
-		}
-		pw.flush();
-}
 	
-	static PrintWriter pw;
-	static MScanner s;
-	static class MScanner {
-		StringTokenizer st;
-		BufferedReader br;
-		public MScanner(InputStream system) {
-			br = new BufferedReader(new InputStreamReader(system));
-		}
-	 	public MScanner(String file) throws Exception {
-			br = new BufferedReader(new FileReader(file));
-		}
-	 
-		public String next() throws IOException {
-			while (st == null || !st.hasMoreTokens())
-				st = new StringTokenizer(br.readLine());
-			return st.nextToken();
-		}
-		public int[] intArr(int n) throws IOException {
-			int[] in = new int[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			return in;
-		}
-		public void pArr(int[] in,int n) throws IOException {
-			
-			for (int i = 0; i < n; i++)
-				System.out.println(in[i]);
-			
-		}
-		public void pLis(ArrayList<Integer> l) throws IOException {
-			
-			for (int i = 0; i < l.size(); i++)
-				System.out.println(l.get(i));
-			
-		}
-	 	public long[] longArr(int n) throws IOException {
-			long[] in = new long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			return in;
-		}
-		public int[] intSortedArr(int n) throws IOException {
-			int[] in = new int[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			shuffle(in);
-			Arrays.sort(in);
-			return in;
-		}
-		public long[] longSortedArr(int n) throws IOException {
-			long[] in = new long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			shuffle(in);
-			Arrays.sort(in);
-			return in;
-		}
-	 	public Integer[] IntegerArr(int n) throws IOException {
-			Integer[] in = new Integer[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextInt();
-			return in;
-		}
-		public Long[] LongArr(int n) throws IOException {
-			Long[] in = new Long[n];
-			for (int i = 0; i < n; i++)
-				in[i] = nextLong();
-			return in;
-		}
-		public String nextLine() throws IOException {
-			return br.readLine();
-		}
-		public int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
-		public double nextDouble() throws IOException {
-			return Double.parseDouble(next());
-		}
-		public char nextChar() throws IOException {
-			return next().charAt(0);
-		}
-		public long nextLong() throws IOException {
-				return Long.parseLong(next());
-		}
-	 	public boolean ready() throws IOException {
-				return br.ready();
-		}
-		public void waitForInput() throws InterruptedException {
-				Thread.sleep(3000);
-		}
-	}
-		static void shuffle(int[] in) {
-			for (int i = 0; i < in.length; i++) {
-				int idx = (int) (Math.random() * in.length);
-				int tmp = in[i];
-				in[i] = in[idx];
-				in[idx] = tmp;
-			}
-	}
-	 	static void shuffle(long[] in) {
-	 		for (int i = 0; i < in.length; i++) {
-				int idx = (int) (Math.random() * in.length);
-				long tmp = in[i];
-				in[i] = in[idx];
-				in[idx] = tmp;
-			}
-		}
-}
